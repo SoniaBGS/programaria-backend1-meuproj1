0b06a5dd4c4e3a027ff8f1f6be7021f634ba52be
SoniaBGS <soniabgs@gmail>
2023-07-04 03:14:03 -0300

    Atualiza modificacoes

--- mulheres.js
+++ mulheres.js
@@ -1,14 +1,15 @@
 const express = require("express") // aqui estou iniciando o express
 const router = express.Router() //aqui estou configurando aprimeira parte da rota
 const cors = require('cors') // aqui estou trazendo o pacote cors que permite consumir essa api no front end
-
 const conectaBancoDeDados = require('./bancoDeDados') // aqui iniciando o banco de dados
 conectaBancoDeDados()
+
 const Mulher = require('./mulherModel')
 
 const app = express() //aqui estou iniciando o app
 app.use(express.json()) //
 app.use(cors())
+
 const porta = 3333; // aqui estou criando a porta
 
 // GET
@@ -89,3 +90,4 @@ function mostraPorta() {
 }
 
 app.listen(porta, mostraPorta); // servidor ouvindo a porta
+
